dfe35c791f10e35adbee1a9b5a6a19bab672bccc
Arne <Arne@Arnes-MacBook-Air.local>
2016-03-05 14:04:48 +0100

    Restructure

--- widget/src/osc/IRreset.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'OSC.php';
-include 'settings.php';
-$c = new OSCClient();
-$c->set_destination($IRserver,$IRport);
-$c->send(new OSCMessage("/ir", array(0)));
-echo "$";
-echo $_GET["v"];
-?>
--- widget/src/osc/RFreset.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'OSC.php';
-include 'settings.php';
-$c = new OSCClient();
-$c->set_destination($RFserver,$RFport);
-$c->send(new OSCMessage("/rf", array(0,0,0)));
-echo "$";
-echo $_GET["v"];
-?>
